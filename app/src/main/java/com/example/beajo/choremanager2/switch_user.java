501c06836610850e3740a03fc8abf24abc8c4d52
Beatrice084 <bjohn084@uottawa.ca>
2017-11-24 10:10:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/beajo/choremanager2/switch_user.java
@@ -0,0 +1,13 @@
+package com.example.beajo.choremanager2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class switch_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_switch_user);
+    }
+}
