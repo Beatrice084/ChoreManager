fedcce6f20527e38ca5e50f21aab44522d4ae117
saheed <kingsaheed@icloud.com>
2017-11-27 19:47:08 -0500

    a working db

---
+++ app/src/main/java/com/example/beajo/choremanager2/MyDBHandler.java
@@ -0,0 +1,338 @@
+package com.example.beajo.choremanager2;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.content.ContentValues;
+import android.database.Cursor;
+import java.util.ArrayList;
+
+/**
+ * Created by saheed on 2017-11-27.
+ */
+
+
+// make this a singleton class
+
+public class MyDBHandler extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "choreApp.db";
+
+    public MyDBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // creating table for the list of family members
+        String CREATE_PERSON_TABLE = "CREATE TABLE Person (" +
+                "name TEXT PRIMARY KEY UNIQUE KEY NOT NULL)";
+
+
+        // creating task table
+        String CREATE_TASK_TABLE = "CREATE TABLE Task (" +
+                "taskName TEXT NOT NULL, " +
+                "personAssigned TEXT NOT NULL, " +
+                "taskNote TEXT," +
+                // have to figure out how to relate equipment to tasks ""+
+                "CONSTRAINT personTask FOREIGN KEY (personAssigned) REFERENCES Person (name))";
+
+
+
+        // creating cupboard and fride table
+        String CREATE_CUPBOARDANDFRIDGEITEM_TABLE ="CREATE TABLE Cupboardandfridgeitem (" +
+                "item TEXT NOT NULL," +
+                "CONSTRAINT personCupboard FOREIGN KEY (person) REFERENCES Person (name))";
+
+        // creating table for shopping items
+        String CREATE_SHOPPINGITEM_TABLE = "CREATE TABLE Shoppingitem (" +
+                "person TEXT NOT NULL," +
+                "shoppingitem TEXT NOT NULL," +
+                "CONSTRAINT personShoppingitem FOREIGN KEY (person) REFERENCES Person (name)";
+
+        // creating table for tools
+        String CREATE_TOOLS_TABLE ="CREATE TABLE Tool (item TEXT PRIMARY KEY UNIQUE KEY NOT NULL)";
+
+        db.execSQL(CREATE_PERSON_TABLE);
+        db.execSQL(CREATE_TASK_TABLE);
+        db.execSQL(CREATE_CUPBOARDANDFRIDGEITEM_TABLE);
+        db.execSQL(CREATE_SHOPPINGITEM_TABLE);
+        db.execSQL(CREATE_TOOLS_TABLE);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion,
+                          int newVersion) {
+    }
+
+    // controler methods for table person
+    public void addPerson(PersonObject person) {
+
+        ContentValues values = new ContentValues();
+        values.put("name", person.getName());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert("Person", null, values);
+
+        db.close();
+
+    }
+    public PersonObject findPerson(String name) {
+        String query = "Select * FROM Person WHERE name = \"" +name+ "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        PersonObject person = null;
+        if (cursor.moveToFirst()) {
+            person = new PersonObject(cursor.getString(0));
+            cursor.close();
+        }
+        db.close();
+        return person;
+    }
+    public boolean deletePerson(String name) {
+        boolean result = false;
+
+        String query = "Select * FROM Person WHERE name = \"" +name+ "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            String nameToDelete = cursor.getString(0);
+            db.delete("Person", "name = " + nameToDelete, null);
+            cursor.close();
+            result = true;
+        }
+
+        db.close();
+        return result;
+    }
+
+    // controller methods for table task
+    public void addTask(PersonObject person,TaskItemObject task) {
+
+        ContentValues values = new ContentValues();
+        values.put("taskName",task.getName());
+        values.put("personAssigned", person.getName());
+        values.put("taskNote",task.getNote());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert("Task", null, values);
+
+        db.close();
+
+    }
+    public ArrayList<TaskItemObject> findTask(PersonObject person) {
+        String query = "Select * FROM Task WHERE personAssigned = \"" +person.getName()+ "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        ArrayList<TaskItemObject> result = new ArrayList<TaskItemObject>();
+
+        for (int i=0 ;i<cursor.getCount();i++){
+            if(cursor.moveToPosition(i)){
+                result.add(new  TaskItemObject(cursor.getString(0),new PersonObject( cursor.getString(1)),cursor.getString(2),null));
+            }
+        }
+        cursor.close();
+        db.close();
+        return result;
+    }
+    public ArrayList<TaskItemObject> findTask() {
+        String query = "Select * FROM Task ";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        ArrayList<TaskItemObject> result = new ArrayList<TaskItemObject>();
+
+        for (int i=0 ;i<cursor.getCount();i++){
+            if(cursor.moveToPosition(i)){
+                result.add(new  TaskItemObject(cursor.getString(0),new PersonObject( cursor.getString(1)),cursor.getString(2),null));
+            }
+        }
+        cursor.close();
+        db.close();
+        return result;
+    }
+    public boolean deleteTask(String taskName) {
+        boolean result = false;
+
+        String query = "Select * FROM Task WHERE taskName = \"" +taskName+ "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            String taskToDelete = cursor.getString(1);
+            db.delete("Task", "taskName = " + taskToDelete, null);
+            cursor.close();
+            result = true;
+        }
+
+        db.close();
+        return result;
+    }
+
+
+
+    // controller methods for cupboard item
+    public void addCupboardItem(ItemObject item) {
+
+        ContentValues values = new ContentValues();
+        values.put("item", item.getName());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert("Cupboardandfridgeitem", null, values);
+
+        db.close();
+
+    }
+    public ArrayList<ItemObject> findCupboardItem() {
+        String query = "Select * FROM Cupboardandfridgeitem" ;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        ArrayList<ItemObject> result = new ArrayList<ItemObject>();
+
+
+        for (int i=0 ;i<cursor.getCount();i++){
+            if(cursor.moveToPosition(i)){
+                result.add(new  ItemObject(cursor.getString(0)));
+            }
+        }
+        cursor.close();
+        db.close();
+        return result;
+    }
+    public boolean deleteCupboardItem(String item) {
+        boolean result = false;
+
+        String query = "Select * FROM Cupboardandfridgeitem WHERE item = \"" +item+ "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            String itemToDelete = cursor.getString(0);
+            db.delete("Cupboardandfridgeitem", "item = " + itemToDelete, null);
+            cursor.close();
+            result = true;
+        }
+
+        db.close();
+        return result;
+    }
+
+
+
+
+    // controller methods for shopping items
+    public void addShoppingItem(PersonObject person,ItemObject item) {
+
+        ContentValues values = new ContentValues();
+        values.put("person", person.getName());
+        values.put("item", item.getName());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert("Shoppingitem", null, values);
+
+        db.close();
+
+    }
+    public ArrayList<ItemObject> findShoppingItems(PersonObject person){
+        String query = "Select * FROM Shoppingitem WHERE person =  \"" +person.getName()+ "\""  ;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        ArrayList<ItemObject> result = new ArrayList<ItemObject>();
+
+
+        for (int i=0 ;i<cursor.getCount();i++){
+            if(cursor.moveToPosition(i)){
+                result.add(new  ItemObject(cursor.getString(1)));
+            }
+        }
+        db.close();
+        cursor.close();
+        return result;
+    }
+    public boolean deleteShoppingItem(ItemObject item) {
+        boolean result = false;
+
+        String query = "Select * FROM Task WHERE name = \"" +item.getName()+ "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            String taskToDelete = cursor.getString(1);
+            db.delete("Task", "taskName = " + taskToDelete, null);
+            cursor.close();
+            result = true;
+        }
+
+        db.close();
+        return result;
+    }
+
+    // controller methods for tools
+    public void addTool(ItemObject item) {
+
+        ContentValues values = new ContentValues();
+        values.put("item", item.getName());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert("Tool", null, values);
+
+        db.close();
+
+    }
+    public ArrayList<ItemObject> findTools() {
+        String query = "Select * FROM Tool" ;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        ArrayList<ItemObject> result = new ArrayList<ItemObject>();
+
+
+        for (int i=0 ;i<cursor.getCount();i++){
+            if(cursor.moveToPosition(i)){
+                result.add(new  ItemObject(cursor.getString(0)));
+            }
+        }
+        cursor.close();
+        db.close();
+        return result;
+    }
+    public boolean deleteTool(String item) {
+        boolean result = false;
+
+        String query = "Select * FROM Tool WHERE item = \"" +item+ "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            String itemToDelete = cursor.getString(0);
+            db.delete("Tool", "item = " + itemToDelete, null);
+            cursor.close();
+            result = true;
+        }
+
+        db.close();
+        return result;
+    }
+}
---
+++ app/src/main/java/com/example/beajo/choremanager2/PersonObject.java
@@ -0,0 +1,20 @@
+package com.example.beajo.choremanager2;
+
+/**
+ * Created by saheed on 2017-11-26.
+ */
+
+public class PersonObject {
+    private String name;
+    public PersonObject(String name){
+        this.name = name;
+
+
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
