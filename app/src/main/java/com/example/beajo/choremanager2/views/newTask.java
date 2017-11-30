6e8072952dc71b7d6a107163d6981d872e806670
ayuille <ayuil040@uottawa.ca>
2017-11-29 21:38:50 -0500

    person individual name

---
+++ app/src/main/java/com/example/beajo/choremanager2/views/newTask.java
@@ -0,0 +1,15 @@
+package com.example.beajo.choremanager2.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.beajo.choremanager2.R;
+
+public class newTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+    }
+}
