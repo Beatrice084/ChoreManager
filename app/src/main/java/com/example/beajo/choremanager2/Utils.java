6f0f4a9beafd474bc61d26fd809b347db3bdf2e0
saheed <kingsaheed@icloud.com>
2017-11-29 20:48:32 -0500

    tasks

--- app/src/main/java/com/example/beajo/choremanager2/Utils.java
+++ app/src/main/java/com/example/beajo/choremanager2/Utils.java
@@ -1,8 +1,12 @@
 package com.example.beajo.choremanager2;
 
 import com.example.beajo.choremanager2.model.Person;
+import com.example.beajo.choremanager2.model.TaskItem;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 
 /**
  * Created by oguns on 11/29/2017.
@@ -10,13 +14,29 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Utils {
     private DatabaseReference mDatabase;
-
+    private static ArrayList<TaskItem> tasks;
     public Utils() {
+
+
+
        mDatabase  = FirebaseDatabase.getInstance().getReference();
+        tasks = null;
     }
 
     public void addUser(Person p){
         DatabaseReference peopleReference = mDatabase.child("People/"+p.getUid());
         peopleReference.setValue(p);
+    }
+    public static ArrayList<TaskItem> getTasks(String uid){
+        ArrayList<TaskItem> myTasks = new ArrayList<>();
+        Iterator<TaskItem> iterator = tasks.iterator();
+        while(iterator.hasNext()){
+            TaskItem current = iterator.next();
+            if(current.getPersonAssigned().getUid().equals(uid)){
+                myTasks.add(current);
+            }
+        }
+
+        return myTasks;
     }
 }
