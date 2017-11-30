c4c90a7ac433f7dff35247e6eb77e3ea5f3f2f95
ayuille <ayuil040@uottawa.ca>
2017-11-29 21:03:59 -0500

    people list

--- app/src/main/java/com/example/beajo/choremanager2/views/PersonIndividualActivity.java
+++ app/src/main/java/com/example/beajo/choremanager2/views/PersonIndividualActivity.java
@@ -2,14 +2,31 @@ package com.example.beajo.choremanager2.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.beajo.choremanager2.R;
+import com.example.beajo.choremanager2.model.Person;
 
 public class PersonIndividualActivity extends AppCompatActivity {
+    private ListView currentTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_individual);
+        Bundle bundle = getIntent().getExtras();
+        Person p = bundle.getParcelable("person");
+
+        TextView t = new TextView(this);
+        t=(TextView)findViewById(R.id.personName2);
+
+        t.setText(p.getName());
+
+        currentTasks = (ListView)findViewById(R.id.currentList);
+
+
+
+
     }
 }
