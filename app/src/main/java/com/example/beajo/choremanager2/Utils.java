05046e025ab2a20a5068672cb7b96a69ef5d471a
ToluwaniO <ogunsanyatoluwani@gmail.com>
2017-11-29 20:46:48 -0500

    get people from firebase

--- app/src/main/java/com/example/beajo/choremanager2/Utils.java
+++ app/src/main/java/com/example/beajo/choremanager2/Utils.java
@@ -1,8 +1,15 @@
 package com.example.beajo.choremanager2;
 
+import android.util.Log;
+
 import com.example.beajo.choremanager2.model.Person;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 /**
  * Created by oguns on 11/29/2017.
@@ -10,6 +17,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Utils {
     private DatabaseReference mDatabase;
+    private static ArrayList<Person> people = new ArrayList<>();
+    private static String TAG = Utils.class.getSimpleName();
 
     public Utils() {
        mDatabase  = FirebaseDatabase.getInstance().getReference();
@@ -18,5 +27,38 @@ public class Utils {
     public void addUser(Person p){
         DatabaseReference peopleReference = mDatabase.child("People/"+p.getUid());
         peopleReference.setValue(p);
+    }
+
+    public void getPeople(){
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Person p = dataSnapshot.getValue(Person.class);
+                Log.d(TAG, "childAdded");
+                people.add(p);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        DatabaseReference peopleReference = mDatabase.child("People");
+        peopleReference.addChildEventListener(childEventListener);
     }
 }
